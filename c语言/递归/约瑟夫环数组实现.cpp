f3c2df823b48ea4046f75994f7f09af279142340
yixin <yixincode@163.com>
2024-03-14 15:58:10 +0800

    约瑟夫环

---
+++ c语言/递归/约瑟夫环数组实现.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+void joseph(std::vector<int> &vec, int peoples, int rule);
+
+int main() {
+    std::vector<int> vec;
+
+    return 0;
+}
+
+void joseph(std::vector<int> &vec, int peoples, int rule) {
+    
+}
