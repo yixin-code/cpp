77018e85c7d07893ab3352eb1d1d51bd641f7abe
yixin <yixincode@163.com>
2024-04-10 17:06:37 +0800

    test

--- 练习/两个有序数组相同元素.cpp
+++ 练习/两个有序数组相同元素.cpp
@@ -4,6 +4,19 @@
 int main() {
     std::vector<int> vec{1, 2, 3, 4, 5, 6};
     std::vector<int> vec2{4, 5, 6, 7, 8};
+    std::vector<int> v_res;
+
+    int i = 0;
+    int j = 0;
+
+    while (i != vec.size() || j != vec2.size()) {
+        if (vec[i] == vec2[j]) {
+            ++i;
+            ++j;
+        } else if (vec[i] < vec2[j]) {
+            ++i;
+        }
+    }
 
     return 0;
 }
