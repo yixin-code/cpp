cc5c6c0d0b6921aa759e6799121e3ab44afaf22c
yixin <yixincode@163.com>
2024-04-28 17:04:35 +0800

    log

--- my_nignx/_include/ngx_macro.h
+++ my_nignx/_include/ngx_macro.h
@@ -9,4 +9,15 @@
 #define MAX_INT64_LEN (sizeof("-9223372036854775808") - 1) // 64位整数最长字符, -1除去\0
 #define MAX_UINT32 (u_int32_t)0xffffffff // 32位无符号最大整数
 
+// 日志级别
+#define NGX_LOG_STDERR      0 //控制台错误【stderr】：最高级别日志，日志的内容不再写入log参数指定的文件，而是会直接将日志输出到标准错误设备比如控制台屏幕
+#define NGX_LOG_EMERG       1 //紧急 【emerg】
+#define NGX_LOG_ALERT       2 //警戒 【alert】
+#define NGX_LOG_CRIT        3 //严重 【crit】
+#define NGX_LOG_ERR         4 //错误 【error】：属于常用级别
+#define NGX_LOG_WARN        5 //警告 【warn】：属于常用级别
+#define NGX_LOG_NOTICE      6 //注意 【notice】
+#define NGX_LOG_INFO        7 //信息 【info】
+#define NGX_LOG_DEBUG       8 //调试 【debug】：最低级别
+
 #endif
