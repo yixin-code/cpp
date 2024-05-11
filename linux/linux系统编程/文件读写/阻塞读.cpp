f2354d7865a19abc6c2e0dd5c17a65dcdc314b46
yixin <yixincode@163.com>
2024-05-12 00:26:07 +0800

    read

--- linux/linux系统编程/文件读写/阻塞读.cpp
+++ linux/linux系统编程/文件读写/阻塞读.cpp
@@ -2,7 +2,11 @@
 #include <fcntl.h> // open
 
 int main(int argc, char *argv[]) {
-    int fd = open("/dev/tty", O_RDONLY | ); // 终端 对其读写
+    int fd = open("/dev/tty", O_RDONLY | O_NONBLOCK); // 终端 非阻塞读终端
+    if (fd == -1) {
+        perror("5 open");
+        exit(1);
+    }
 
     return 0;
 }
