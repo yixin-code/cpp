0cd0d1901c98b163c07ea6d8bd30a3461744d9d1
yixin <yixincode@163.com>
2024-06-06 18:12:10 +0800

    pthread

--- linux/linux系统编程/线程/read_write.cpp
+++ linux/linux系统编程/线程/read_write.cpp
@@ -40,9 +40,29 @@ CReadWrite::~CReadWrite() {
 void CReadWrite::write(void) {
     for (int i = 1; i <= 11; ++i) {
         pthread_mutex_lock(&this->m_mutex_r);
+        this->m_data    = i;
+        this->m_flag_r  = true;
+        std::cout << "make data: " << this->m_data << std::endl;
+        pthread_cond_broadcast(&this->m_cond_r);
+        pthread_mutex_unlock(&this->m_mutex_r);
+
+        pthread_mutex_lock(&this->m_mutex_w);
+        while (this->m_flag_w == false) {
+            pthread_cond_wait(&this->m_cond_w, &this->m_mutex_w);
+        }
+        this->m_flag_w  = false;
+        pthread_mutex_unlock(&this->m_mutex_w);
     }
 }
 
 void CReadWrite::read(void) {
-
+    for (int i = 1; i <= 11; ++i) {
+        pthread_mutex_lock(&this->m_mutex_r);
+        while (this->m_flag_r == false) {
+            pthread_cond_wait(&this->m_cond_r, &this->m_mutex_r);
+        }
+        this->m_flag_r = false;
+        std::cout << "process data: " << this->m_data << std::endl;
+        pthread_mutex_unlock(&this->m_mutex_r);
+    }
 }
