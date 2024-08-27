dd5327ef603456f8e66312d5613154448d95a414
yixin <yixincode@163.com>
2024-08-27 09:46:30 +0800

    test

--- 练习/test.cpp
+++ 练习/test.cpp
@@ -1,11 +1,38 @@
 #include <iostream>
+#include <pthread.h> // pthread_t pthread_create pthread_exit pthread_self
 
-void func() {
+int num = 0;
 
+void* func(void *arg) {
+    for (int i = 0; i < 50000; ++i) {
+        // ++num;
+        num = num + 1;
+        std::cout << "tid: " << pthread_self() << ", num: " << num << '\n';
+    }
+
+    pthread_exit(nullptr);
+}
+
+void work() {
+    pthread_t   tid   = 0;
+    pthread_t   tid2  = 0;
+
+    if (pthread_create(&tid, nullptr, func, nullptr) != 0) {
+        perror("pthread_create tid");
+        exit(1);
+    }
+
+    if (pthread_create(&tid2, nullptr, func, nullptr) != 0) {
+        perror("pthread_create tid2");
+        exit(1);
+    }
+
+    pthread_join(tid, nullptr);
+    pthread_join(tid2, nullptr);
 }
 
 int main(int argc, char *argv[]) {
-    func();
+    work();
 
     return 0;
 }
